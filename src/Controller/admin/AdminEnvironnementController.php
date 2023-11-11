587a51e25b8a69cc78e7a3873452e0261d52957a
Utilisateur 1 <Utilisateur 1@LAPTOP-UH110N76>
2023-11-11 05:48:22 +0100

    Phase 16 : ORM (gestion des environnements : ajout et suppression).

---
+++ src/Controller/admin/AdminEnvironnementController.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Controller\admin;
+
+use App\Entity\Environnement;
+use App\Repository\EnvironnementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminEnvironnementController
+ *
+ * @author Utilisateur 1
+ */
+
+class AdminEnvironnementController extends AbstractController {
+
+    /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param EnvironnementRepository $repository
+     */
+    public function __construct(EnvironnementRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/admin/environnements", name="admin.environnements")
+     * @return Response
+     */
+    public function index(): Response{
+        $environnements = $this->repository->findAll();
+        return $this->render("admin/admin.environnements.html.twig", [
+            'environnements' => $environnements
+        ]);
+    }
+
+    /**
+     * @Route("/admin/environnement/suppr/{id}", name="admin.environnement.suppr")
+     * @param Environnement $environnement
+     * @param int $id
+     * @return Response
+     */
+    public function suppr(int $id): Response{
+        $environnement = $this->repository->find($id);
+        $this->repository->remove($environnement, true);
+        return $this->redirectToRoute('admin.environnements');
+    }
+
+    /**
+     * @Route("/admin/environnement/ajout", name="admin.environnement.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response{
+        $nomEnvironnement = $request->get("nom");
+        $environnement = new Environnement();
+        $environnement->setNom($nomEnvironnement);
+        $this->repository->add($environnement, true);
+        return $this->redirectToRoute('admin.environnements');       
+    }        
+
+}
